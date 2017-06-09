4d0163f2817a5d7e1e2ef075fc50dffdccf796db
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-06-09 21:28:17 +0900

    yes_no判定のみ

--- main.cpp
+++ main.cpp
@@ -1,6 +1,101 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int n;
+char seed[1010][30] = {0}; //種
+int que[1010][2][30] = {0}; //種.toInt
+bool mem[30] = {0}; //trueの場合使ってはいけない
+
+bool judge()
+{
+    bool updated[1010] = {0};
+    int kind[2][30] = {0};
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            for(int k = 0; k < 26; k++)
+            {
+                kind[j][k] += que[i][j][k];
+            }
+        }
+    }
+    bool flag;
+    while(1)
+    {
+        flag = false;
+        for(int i = 0; i < 26; i++)
+        {
+            if(kind[0][i] == 0 || kind[1][i] == 0)
+            {
+                kind[0][i] = kind[1][i] = -1;
+                mem[i] = true;
+                flag = true;
+            }
+        }
+        if(!flag)
+        {
+            for(int i = 0; i < 26; i++)
+            {
+                if(kind[0][i] > 0 || kind[1][i] > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(updated[i])
+            {
+                continue;
+            }
+            for(int j = 0; j < 26; j++)
+            {
+                if(mem[j] && (que[i][0][j] == 1 || que[i][1][j] == 1))
+                {
+                    for(int l = 0; l < 26; l++)
+                    {
+                        kind[0][l] -= que[i][0][l];
+                        kind[1][l] -= que[i][1][l];
+                    }
+                    updated[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s",seed[i]);
+        //scanf("%s",seed);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < strlen(seed[i]); j++)
+        {
+            if(isupper(seed[i][j]))
+            {
+                que[i][0][seed[i][j] - 'A']++;
+            }
+            else
+            {
+                que[i][1][seed[i][j] - 'a']++;
+            }
+        }
+    }
+    if(!judge())
+    {
+        printf("NO\n");
+    }
+    else
+    {
+        printf("YES\n");
+    }
     return 0;
 }
