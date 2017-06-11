4a6fc6a006b286c7966bb1806a2eb91500560d21
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-06-11 22:26:54 +0900

    fail

--- main.cpp
+++ main.cpp
@@ -67,6 +67,134 @@ bool judge()
     }
 }
 
+vector<int> base,base_;
+vector<int> seed_;
+vector<int> box[30][3];
+bool used[3000] = {0},used_[3000] = {0};
+
+
+void output( int vec[3000] ){
+    int i = 1;
+    string ans;
+    string a,a_;
+    a = seed[vec[0]];
+    while( vec[i] == -1 ){
+        a_ = seed[vec[i]];
+        for( int i = 0; i < a.size(); ++i ){
+            strcat(ans,a[i]);
+        }
+        for( int i = 0; i < a_.size(); ++i ){
+            strcat(ans,a_[i]);
+        }
+        //kaburikesu sort
+        printf("%d %d %c\n",vec[i-1],vec[i],ans);
+    }
+    if( i > 1 ){
+        printf("%d %d %c\n",n+i,n+i,'!');
+    }else{
+        printf("%d %d %s\n",vec[0],vec[0],a);
+    }
+}
+
+void hunachi(){
+
+    for( int i = 0; i < 26 ; ++i ){
+        if( mem[i] == 0 ){
+            base.push_back(i);
+        }else{
+            base_.push_back(i);
+        }
+    }
+    for( int i = 0; i < n ; ++i ){
+        int flag = true;
+        for( int j = 0; j < base_.size(); ++j ){
+            if( que[i][0][j] > 0 ){
+                flag = false;
+            }
+        }
+        if( flag ){
+            seed_.push_back(i);
+        }
+    }
+
+    for( int i = 0; i < seed_.size() ; ++i ){
+        for( int j = 0; j < 26 ; ++j ){
+            if( que[seed_[i]][0][j] || que[seed_[i]][1][j] ){
+                box[j][2].push_back(i);
+            }else if( que[seed_[i]][0][j] ){
+                box[j][0].push_back(i);
+            }else if( que[seed_[i]][1][j] ){
+                box[j][1].push_back(i);
+            }
+        }
+    }
+    /*
+    int box_count[30] = {0};
+    */
+    int num_min = seed_.size();
+    int cnt = 0;
+    //while(1){
+        int ans[3000] = {0};
+        set<int> small,big,comp;
+        for( int i = 0 ; i < 26 ; ++i ){
+            if( box[i][2].size() > 0 ){
+                //int sel = box[i][2][0];
+                small.insert(i);
+                while( small.size() || big.size() ){
+                    int num = ( small.size() == 0 ) ? small.begin : big.begin();
+                    int sel,sel_;
+                    bool two = false;
+                    if( box[num][2].size() > 0 ){
+                        sel = box[num][2][0];
+                        ans[cnt] = num;
+                        cnt++;
+                    }else{
+                        two =true;
+                        sel = box[num][0][0];
+                        ans[cnt] = sel;
+                        cnt++;
+                        sel_ = box[num][1][0];
+                        ans[cnt] = sel;
+                        cnt++;
+                    }
+                    for( int j = 0 ; j < 26 ; ++j ){
+                        if( que[sel][0][j] && que[sel][1][j] ){
+                            small.erase(j);
+                            big.erase(j);
+                            comp.insert(j);
+                        }else if( que[sel][0][j] ){
+                            if( small.erase(j) )continue;
+                            small.insert(j);
+                        }else if( que[sel][1][j] ){
+                            if( big.erase(j) )continue;
+                            big.insert(j);
+                        }
+                    }
+                    if( two ){
+                        for( int j = 0 ; j < 26 ; ++j ){
+                            if( que[sel_][0][j] && que[sel_][1][j] ){
+                                small.erase(j);
+                                big.erase(j);
+                                comp.insert(j);
+                            }else if( que[sel_][0][j] ){
+                                if( small.erase(j) )continue;
+                                small.insert(j);
+                            }else if( que[sel_][1][j] ){
+                                if( big.erase(j) )continue;
+                                big.insert(j);
+                            }
+                        }
+                    }
+                }
+                comp.clear();
+                output(ans);
+                cnt = 0;
+
+            }
+        }
+    //}
+}
+
 int main()
 {
     scanf("%d",&n);
