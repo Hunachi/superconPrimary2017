d54689b7c0777e98095599c705fc0016f14293c0
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-06-14 04:54:01 +0900

    ほけ＾

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int INF = 30000;
 vector<int> big[30],small[30],comp[30];
 vector<pair<int,int>> comp_num[30][2];
 vector<unsigned long long> comp_bit[30][2];
-vector<int> answer;
+vector<int> answer,answer2;
 
 void hunachi() {
 
@@ -70,17 +70,17 @@ void hunachi() {
                 comp_num[j][0].push_back(make_pair(-1,j));
                 big[j].push_back(i);
                 small[j].push_back(i);
-                break;
+                //break;
             }
             else if (que[i][0][j])
             {
                 big[j].push_back(i);
-                break;
+                //break;
             }
             else if (que[i][1][j])
             {
                 small[j].push_back(i);
-                break;
+                //break;
             }
         }
 
@@ -112,16 +112,18 @@ void prepare(){
                         o++;
                     }
                 }
-                if (o < c) {
+                if (o > 0 && o < c) {
                     c = o;
                     s = k;
-                    comp_bit[i][1].push_back((seed_bit[j] | seed_bit[o]));
                 }
             }
-            comp_num[i][1].push_back(make_pair(j, s));
+            if(!small[i].empty()){
+                comp_bit[i][1].push_back((seed_bit[j] | seed_bit[s]));
+                comp_num[i][1].push_back(make_pair(j, s));
+            }
         }
     }
-    /*
+/*
     for(int i : base){
         cout << i <<" : "<< endl;
         cout << "comp 1 = " ;
@@ -144,9 +146,218 @@ void prepare(){
     }*/
 }
 
-void narahara(){
-    for(int i : base){
+void out(unsigned long long j){
+    char c;
+    for(int k : base){
+        if((j >> (k * 2)) & 1){
+           c = (char)('A' + k);
+            cout << c;
+        }
+        if((j >> (k * 2 + 1)) & 1){
+            c = (char)('a' + k);
+            cout << c;
+        }
+    }
+    cout << endl;
+    return;
+}
 
+bool check(unsigned long long j){
+    for(int k : base)
+    {
+        bool b = (bool)(j >> (k * 2) & 1);
+        bool c = (bool)(j >> (k * 2 + 1)& 1);
+        if(c != b)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void mya(){
+    int ans = seed_.size();
+    set<int> a;
+    for(int i = 0; i < base.size(); i++)
+    {
+        int p = base[i];
+        int num = 0;
+        if(comp_num[p][num].empty())num++;
+        int ans_;
+        //cout << "i " << p << endl;
+        for(int h = num ; h < 2; h++ )
+        {
+            //cout << "h " << h << endl;
+            int count = 0;
+            for (unsigned long long j : comp_bit[p][h])
+            {
+                answer.push_back(p);
+                //cout << j << " = j"<< endl;
+                ans_= h + 1;
+                if(ans_ == 2)
+                {
+                    //cout << p << " ans = " << count <<endl;
+                    if(comp_num[p][h].size() <= count){
+                        continue;
+                    }
+                    //cout << comp_num[p][h][count].first << " " << comp_num[p][h][count].second<< " ";
+                    answer.push_back(comp_num[p][h][count].second);
+                    answer.push_back(comp_num[p][h][count].first);
+                }
+                else
+                {
+                    answer.push_back(comp_num[p][h][count].second);
+                }
+                for (int k : base)
+                {
+                    bool b = (bool) (j >> (k * 2) & 1);
+                    bool c = (bool) (j >> (k * 2 + 1) & 1);
+                    if (c != b)
+                    {
+                        if (b)
+                        {
+                            a.insert(k * 2);
+                        }
+                        if (c)
+                        {
+                            a.insert(k * 2 + 1);
+                        }
+                    }
+                }
+                unsigned long long p1 = j;
+
+                bool flags = true;
+                ///cout << a.size() << " = a.size()" << endl;
+                while (!a.empty() && flags)
+                {
+                    ans_++;
+                    auto kk = a.begin();
+                    int k = *kk;
+                    //cout << " k " << k << endl;
+                    a.erase(k);
+                    int c = k / 2;
+                    int saisho = 52;
+                    int ans_nu = -1;
+                    //cout << "front of k = " ;
+                    if ((k % 2) == 1)
+                    {
+                        //cout << "big" << c <<endl;
+                        for (int l : big[c])
+                        {
+                            unsigned long long p2 = (p1 | seed_bit[l]);
+                            p2 = (p2 - p1);
+                            int o = __builtin_popcount(p2);
+                            //cout << o << endl;
+                            if (o < saisho)
+                            {
+                                //cout << o << " o " << endl;
+                                saisho = o;
+                                ans_nu = l;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int l : small[c])
+                        {
+                            unsigned long long p2 = (p1 | seed_bit[l]);
+                            p2 = p2 - p1;
+                            int o = __builtin_popcount(p2);
+                            if (o < saisho)
+                            {
+
+                                //cout << " o " << o << endl;
+                                saisho = o;
+                                ans_nu = l;
+                            }
+                        }
+                    }
+                    if(ans_nu == -1)
+                    {
+                        //存在しない
+                        //cout << "NG" << c << endl;
+                        flags = false;
+                        a.clear();
+                    }
+                    /*bool fla = false;
+                    if(ans_nu.second == -1) {
+                        for (int r = 0; r < 2; r++) {
+                            for (int l = 0; l < comp_bit[i][r].size(); l++) {
+                                if ((l >> k) & 1) {
+                                    unsigned long long p2 = (p1 | l);
+                                    p2 = p2 - p1;
+                                    int o = __builtin_popcount(p2);
+                                    if (o < saisho) {
+                                        if (r == 1) {
+                                            fla = true;
+                                        } else {
+                                            fla = false;
+                                        }
+                                        saisho = o;
+                                        ans_nu = comp_num[i][r][l];
+                                    }
+                                }
+                            }
+                        }
+                    }*/
+                    unsigned long long p3,p4;
+                    p3 = (seed_bit[ans_nu] | p1);
+                    p4 = p3;
+                    answer.push_back(ans_nu);
+                    p3 = p3 - p1;
+                    for (int z : base)
+                    {
+                        if ((p3 >> (z * 2)) & 1)
+                        {
+                            bool aa = (bool)a.erase(z * 2);
+                            if(!aa){
+                                a.insert(z * 2);
+                            }
+                        }
+                        if ((p3 >> (z * 2 + 1)) & 1)
+                        {
+                            bool aa = (bool)a.erase(z * 2 + 1);
+                            if(!aa){
+                                a.insert(z * 2 + 1);
+                            }
+                        }
+                    }
+                    //cout << ans_nu << endl;
+                    p1 = p4;
+                }
+                if(ans_ <= ans && flags)
+                {
+                    ans = ans_;
+                    copy(answer.begin(),answer.end(),back_inserter(answer2));
+                }
+                for(int o : answer){
+                    cout << o << " ";
+                }
+                cout << endl;
+                answer.clear();
+                a.clear();
+                count++;
+            }
+        }
+    }
+    cout << "m = " << ans << endl;
+    unsigned long long j = 0;
+    for(int i : answer2){
+        //cout << i << " " ;
+        out(seed_bit[i]);
+        j = (j | seed_bit[i]);
+    }
+    for(int i = 0; i < 26; i++){
+        if((j >> (i * 2))&1){
+            cout << "1 ";
+        }else{
+            cout << "0 ";
+        }
+        if((j >> (i * 2 + 1))&1){
+            cout << "1" << endl;
+        }else{
+            cout << "0" << endl;
+        }
     }
 }
 
@@ -241,6 +452,7 @@ int main()
         printf("YES\n");
         hunachi();
         prepare();
+        mya();
     }
     return 0;
 }
